65a10ad37349edd9798aade72eb7e8ca9a5103f9
Munni <munnirunnisab@gmail.com>
2025-09-22 16:50:01 +1000

    commits

--- tests/SauceDemoApp.spec.js
+++ tests/SauceDemoApp.spec.js
@@ -2,5 +2,7 @@ const { test, expect } = require('@playwright/test');
 test('SauceDemo E2E: login, verify products, add to cart, checkout', async ({ page }) => {
     // 1. Login
     await page.goto('https://www.saucedemo.com/');
+
+    // this is from feature branch 
     
 });
